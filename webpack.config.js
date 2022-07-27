efb8b1b198f4b4f5833dedc51df3369944e15301
Jesus David Muñoz Martinez <david.munoz@sonatafy.com>
2022-07-26 21:20:10 -0500

    Updating npm version in examples

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   devtool: 'source-map',
-  mode: 'development',
+  mode: 'production',
   watch: true,
   module: {
     rules: [
