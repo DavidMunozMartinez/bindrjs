bf14205c73b3f640cfae5f23a9f812b4ec8e035f
DavidMunozMartinez <jdavid_9502@outlook.com>
2022-09-17 03:22:13 -0500

    Adding extra infor in docs

--- examples/examples.js
+++ examples/examples.js
@@ -56,7 +56,30 @@ let LeftNavbar = new Bind({
         codeRef: 'Hooks' 
       }
     ],
+    totalDownloads: 0
   },
+  ready: () => {
+    fetch('https://api.npmjs.org/downloads/point/2021-03-17:2022-09-17/bindrjs')
+      .then((response) => response.json())
+      .then(data => {
+        const formatter = Intl.NumberFormat('en', { notation: 'standard' });
+        let increment = 1;
+        let current = 0;
+        let interval = setInterval(() => {
+          if (current < data.downloads) {
+            current = current + increment
+            let val = formatter.format(current);
+            LeftNavbar.bind.totalDownloads = val;
+            increment = increment + 31;
+          } else {
+            if (current > data.downloads) {
+              LeftNavbar.bind.totalDownloads = formatter.format(data.downloads)
+            }
+            clearInterval(interval);
+          }
+        }, 89);
+      });
+  }
 });
 
 setTimeout(() => {
